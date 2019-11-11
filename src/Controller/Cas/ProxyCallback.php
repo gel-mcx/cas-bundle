189a07908c98e83dcd4b3c5a1e28b49d3d0478c4
Pol Dellaiera <pol.dellaiera@protonmail.com>
2019-11-11 22:17:01 +0100

    wip

--- src/Controller/Cas/ProxyCallback.php
+++ src/Controller/Cas/ProxyCallback.php
@@ -22,6 +22,6 @@ class ProxyCallback extends AbstractController
      */
     public function proxyCallbackHandler(CasInterface $casProtocol)
     {
-        return $casProtocol->requestProxyCallback();
+        return $casProtocol->handleProxyCallback();
     }
 }
